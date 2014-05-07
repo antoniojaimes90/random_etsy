e0b8fdd86bedebe3806b74d66d7dca03522a2174
Antonio Jaimes <antoniojaimes90@gmail.com>
2014-05-06 22:45:13 -0700

    Fix html encodes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   	def showProd(my_hash)
+
   		my_hash["results"].each do |results|
 			$title = results["title"]
 			$price = results["price"]
