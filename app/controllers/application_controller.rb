dfcb7d5c016843a1270c8470ef12fc9a9ac2f0c4
Antonio Jaimes <antoniojaimes90@gmail.com>
2014-05-11 22:02:41 -0700

    update js for multim imgaes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < ActionController::Base
   	def multiImg(prodId)
   		@images = Array.new
   		my_images = JSON.parse(open("https://openapi.etsy.com/v2/listings/#{prodId}/images?api_key=vbnyg7rw8596htzyvf1lpm2n").read)
+      $img_total = my_images["count"]
   		my_images["results"].each do |results|
   			@images.push(results["url_fullxfull"])
   		end
